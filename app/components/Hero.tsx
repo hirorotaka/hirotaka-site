34632f5c6a24a49b75add28ca9dfd4286e0526fb
hiroki takada <hirorotaka5@gmail.com>
2024-12-19 19:18:18 +0900

    必要なコンポーネントの初期雛形の読み込み

---
+++ app/components/Hero.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Hero = () => {
+  return <div>Hello</div>;
+};
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import TopButton from './components/TopButton';
+
 export default function Home() {
-  return <div className="text-darkblue bg-darkblue h-10">hello</div>;
+  return (
+    <>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+      <TopButton />
+    </>
+  );
 }
