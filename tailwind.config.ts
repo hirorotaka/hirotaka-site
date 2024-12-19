9aae5349af16860d9c0c2c8c30a20a3330399e4a
hiroki takada <hirorotaka5@gmail.com>
2024-12-19 19:03:37 +0900

    TailwindCSSの初期設定

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      xs: '475px',
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        darkblue: '#00008b',
+        cadetblue: '#5f9ea0',
+        whitesmoke: '#f5f5f5',
       },
     },
   },
