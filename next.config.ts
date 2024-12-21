8a2cc76024f9eb9d294bda9c59f0122cf9bd0980
hiroki takada <hirorotaka5@gmail.com>
2024-12-21 19:44:33 +0900

    画像の読み込み許可対応

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      's.udemycdn.com',
+      'img-c.udemycdn.com',
+      'www.google.com',
+      'm.media-amazon.com',
+      'assets.st-note.com',
+      'res.cloudinary.com',
+      'baseec-img-mng.akamaized.net',
+      'images-fe.ssl-images-amazon.com',
+      'i.ytimg.com',
+      'import.cdn.thinkific.com',
+    ],
+  },
 };
 
 export default nextConfig;
